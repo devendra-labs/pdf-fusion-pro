2d30e7a10b6dd67cf67b2f505efff8998c42e2af
Devendra Yevale <yevaledevendra14@gmail.com>
2026-08-11 10:56:45 +0530

    fix PDF to Word pdfjs worker deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: [
+    "pdfjs-dist",
+    "dommatrix",
+  ],
 };
 
 export default nextConfig;
